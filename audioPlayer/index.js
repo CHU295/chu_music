858dda3b1246ceeda4221b708002476ffeff6dff
chu <chu295@qq.com>
2019-11-20 17:05:42 +0800

    增加进度条，拖拉功能开发中

--- audioPlayer/index.js
+++ audioPlayer/index.js
@@ -8,17 +8,49 @@ template.innerHTML = `
       box-shadow: 0px 0px 9px 3px #ddd;
       border-radius: 10px;
     }
-
     .btn {
       display: inline-block;
       border: 1px solid #ddd;
       padding: 3px 10px;
       cursor: pointer;
     }
+    .progressBar {
+      position: relative;
+      margin: 10px 0;
+    }
+    .bg-line {
+      height: 6px;
+      background: #eaeaea;
+      border-radius: 6px;
+    }
+    .pass-line {
+      position: absolute;
+      top: 0;
+      left: 0;
+      height: 6px;
+      right: 100%;
+      background: #d4d2d2;
+    }
+    .rBtn {
+      display: inline-block;
+      width: 20px;
+      height: 20px;
+      background: #9eb8ff;
+      border-radius: 20px;
+      position: absolute;
+      right: 0;
+      top: 0;
+      transform: translate(50%,-7px);
+    }
   </style>
   <div class="audio-player">
     <h1 class="songName"></h1>
-    <div>进度</div>
+    <div class="progressBar">
+      <div class="bg-line"></div>
+      <div class="pass-line">
+        <span class="rBtn"></span>
+      </div>
+    </div>
     <div>
       <span class="btn preBtn">上一曲</span>
       <span id="playOrpause" class="btn playBtn">暂停</span>
@@ -28,10 +60,11 @@ template.innerHTML = `
 `
 
 window.audioPlayer = {
-  preBtn: null,
-  nextBtn: null,
-  playBtn: null,
-  songName: null,
+  preBtn: null, // 上一曲按钮
+  nextBtn: null, // 下一曲按钮
+  playBtn: null, // 播放/暂停按钮
+  songName: null, // 当前播放歌曲名
+  progressBar: null, // 进度条控制
 }
 class AudioPlayer extends HTMLElement {
   constructor() {
@@ -48,6 +81,7 @@ class AudioPlayer extends HTMLElement {
     audioPlayer.preBtn = audioPlayerTemp.shadowRoot.querySelector('.preBtn')
     audioPlayer.playBtn = audioPlayerTemp.shadowRoot.querySelector('.playBtn')
     audioPlayer.songName = audioPlayerTemp.shadowRoot.querySelector('.songName')
+    audioPlayer.progressBar = audioPlayerTemp.shadowRoot.querySelector('.pass-line')
     this.addEvents()
   }
 
@@ -81,7 +115,31 @@ class AudioPlayer extends HTMLElement {
         audioPlayer.playBtn.innerText = '暂停'
       }
     }
+    audioPlayer.progressBar.onclick = () => {
+      console.log('下一首，开发中')
+    }
   }
 }
 
 window.customElements.define('audio-player', AudioPlayer);
+
+// 进度条
+function getDuration() {
+  let currentTime = Math.floor(audio.currentTime),
+    duration = Math.floor(audio.duration)
+  console.log(currentTime, duration)
+  changeProgressLine(currentTime, duration)
+}
+function changeProgressLine(cur, all) {
+  let progressBarPosition = 1 - (cur / all).toFixed(2)
+  audioPlayer.progressBar.style.right = progressBarPosition * 100 + '%'
+}
+var playStatusInterval
+audio.addEventListener("playing", function () {		//播放状态Doing
+  playStatusInterval = setInterval(() => {
+    getDuration()
+  }, 1000)
+})
+audio.addEventListener("pause", function () {		//播放状态Doing
+  clearInterval(playStatusInterval)
+})
